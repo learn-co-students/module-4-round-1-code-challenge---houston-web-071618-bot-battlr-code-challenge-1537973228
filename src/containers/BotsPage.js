3e5a9a77d0d789fb20a127b1fca2511b6fd2b80a
farverio <github email address>
2018-09-26 10:55:31 -0500

    all functionality working

--- src/containers/BotsPage.js
+++ src/containers/BotsPage.js
@@ -1,11 +1,14 @@
 import React from "react";
 import BotCollection from "./BotCollection"
 import YourBotArmy from "./YourBotArmy"
+import BotSpecs from "../components/BotSpecs"
 
 class BotsPage extends React.Component {
   state = {
     allBots: [],
-    botArmy: []
+    botArmy: [],
+    showBotSpecs: false,
+    selectedBot: null
   }
   
   componentDidMount() {
@@ -22,22 +25,47 @@ class BotsPage extends React.Component {
     
     if (this.state.botArmy.includes(bot)) {
       tempArmy.splice(tempArmy.indexOf(bot), 1)
+      this.setState({
+        ...this.state,
+        botArmy: tempArmy
+      })
     } else {
-      tempArmy.push(bot)
+      this.setState({
+        ...this.state,
+        showBotSpecs: true,
+        selectedBot: bot
+      })
       //should be using spread in setting state not slice. Suboptimal.
     }
-    
-    this.setState({
-      ...this.state,
-      botArmy: tempArmy
-    })
+  }
+  
+  handleDetailClick = action => {
+    if (action === 'back') {
+      this.setState({
+        ...this.state,
+        showBotSpecs: false,
+        selectedBot: null
+      })
+    } else if (action === 'enlist') {
+      const tempArmy = this.state.botArmy.slice()
+      tempArmy.push(this.state.selectedBot)
+      
+      this.setState({
+        ...this.state,
+        botArmy: tempArmy,
+        showBotSpecs: false,
+        selectedBot: null
+      })
+    }
   }
 
   render() {
     return (
       <div>
         <YourBotArmy botArmy={this.state.botArmy} toggleBot={this.toggleBot}/>
-        <BotCollection allBots={this.state.allBots} toggleBot={this.toggleBot}/>
+        {this.state.showBotSpecs 
+          ? <BotSpecs bot={this.state.selectedBot} handleClick={this.handleDetailClick}/> 
+          : <BotCollection allBots={this.state.allBots} toggleBot={this.toggleBot}/>}
       </div>
     );
   }
