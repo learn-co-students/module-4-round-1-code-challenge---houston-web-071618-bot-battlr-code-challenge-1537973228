219ee4080ed7beda6621835f442e46bcebfddbb4
yendai-vo <yendai.vo@hotmail.com>
2018-09-26 11:32:28 -0500

    nope. far from finished...

--- src/containers/BotCollection.js
+++ src/containers/BotCollection.js
@@ -1,15 +1,45 @@
 import React from "react";
 import BotCard from "../components/BotCard";
 
-class BotCollection extends React.Component {
-  //your code here
+class BotCollection extends React.Component {	
+
+	constructor() {
+    super();
+    this.state = {
+      robots: [],
+      // chosenRobot: null
+    }
+	}
+	
+	// const information = (obj) => {
+  //     const arr = [];
+  //     for (let ele in obj) {
+  //       arr.push(`${ele}: ${obj[ele]}`)
+  //     }
+  //     return arr.map((element, i) => <p key={i}>{element}</p>)
+  //   };
+
+    // const allRobots = this.state.robots.map(({name}, i) => {
+    //   return (
+    //   <BotCard key={i} name={name} >
+    //     <img id={i}  src={this.state.robot.url}/>
+    //     {this.state.chosenRobot === this.state.robots[i] ? information(this.state.chosenRobot) : null}
+    //   </BotCard>
+    // );
+    // });
 
   render(){
+		const allRobots = this.state.robots.map(bot => 
+		<BotCard bot={bot} />)
+
   	return (
   	  <div className="ui four column grid">
     		<div className="row">
-    		  {/*...and here..*/}
-    		  Collection of all bots
+					{/* <div><img src={this.props.robots[0].avatar_url} /></div>
+    		  <div>{this.props.robots[0].name}</div>
+					<div>{this.props.robots[0].catchphrase}</div> */}
+					{/* <BotCard name={this.props.robots[0].name}/> */}
+					{allRobots}
     		</div>
   	  </div>
   	);
--- src/containers/BotsPage.js
+++ src/containers/BotsPage.js
@@ -1,12 +1,46 @@
 import React from "react";
+import BotCollection from "./BotCollection";
+import YourBotArmy from "./YourBotArmy";
 
 class BotsPage extends React.Component {
-  //start here with your code for step one
+
+  constructor() {
+    super();
+    this.state = {
+      robots: null,
+      chosenRobot: null
+    }
+  }
+
+  componentDidMount = () => {
+    this.fetchRobots()
+  }
+
+  fetchRobots = () => {
+    fetch('https://bot-battler-api.herokuapp.com/api/v1/bots')
+    .then(res => res.json())
+    .then(robots => {
+      this.addRobotsToState(robots)
+      console.log(robots)
+      debugger
+    })
+  }
+
+  addRobotsToState = (robots) => {
+    this.setState({
+      robots: robots
+    })
+  }
 
   render() {
+    
+		
+
     return (
       <div>
-        {/* put your components here */}
+        <YourBotArmy />
+        {this.state.robots ? 
+        <BotCollection robots={this.state.robots}/> : <p>Loading</p>}
       </div>
     );
   }
