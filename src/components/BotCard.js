ffc4a44d3140541066ae2b5bace166a78d1f25fb
ELehotzky <Lehotzky.Erica@gmail.com>
2018-09-26 11:43:57 -0500

    added click for bot object

--- src/components/BotCard.js
+++ src/components/BotCard.js
@@ -20,12 +20,16 @@ const BotCard = props => {
       botType = <div />;
   }
 
+  this.handleClick = () => {
+    console.log(bot)
+  }
+
   return (
     <div className="ui column">
       <div
         className="ui card"
         key={bot.id}
-        onClick={() => console.log("add code to connect event listener")}
+        onClick={this.handleClick}
       >
         <div className="image">
           <img alt="oh no!" src={bot.avatar_url} />
