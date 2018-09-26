ed832c2187d79a443eb651f10f2794503742871d
farverio <github email address>
2018-09-26 10:32:53 -0500

    hit checkpoint

--- src/containers/BotCollection.js
+++ src/containers/BotCollection.js
@@ -8,8 +8,9 @@ class BotCollection extends React.Component {
   	return (
   	  <div className="ui four column grid">
     		<div className="row">
-    		  {/*...and here..*/}
-    		  Collection of all bots
+    		  {this.props.allBots.map(bot => {
+            return <BotCard key={bot.id} bot={bot} toggleBot={this.props.toggleBot}/>
+          })}
     		</div>
   	  </div>
   	);
--- src/containers/BotsPage.js
+++ src/containers/BotsPage.js
@@ -1,12 +1,43 @@
 import React from "react";
+import BotCollection from "./BotCollection"
+import YourBotArmy from "./YourBotArmy"
 
 class BotsPage extends React.Component {
-  //start here with your code for step one
+  state = {
+    allBots: [],
+    botArmy: []
+  }
+  
+  componentDidMount() {
+    fetch('https://bot-battler-api.herokuapp.com/api/v1/bots')
+      .then(res => res.json())
+      .then(bots => this.setState({
+        ...this.state,
+        allBots: bots
+      }))
+  }
+  
+  toggleBot = bot => {
+    const tempArmy = this.state.botArmy.slice()
+    
+    if (this.state.botArmy.includes(bot)) {
+      tempArmy.splice(tempArmy.indexOf(bot), 1)
+    } else {
+      tempArmy.push(bot)
+      //should be using spread in setting state not slice. Suboptimal.
+    }
+    
+    this.setState({
+      ...this.state,
+      botArmy: tempArmy
+    })
+  }
 
   render() {
     return (
       <div>
-        {/* put your components here */}
+        <YourBotArmy botArmy={this.state.botArmy} toggleBot={this.toggleBot}/>
+        <BotCollection allBots={this.state.allBots} toggleBot={this.toggleBot}/>
       </div>
     );
   }
