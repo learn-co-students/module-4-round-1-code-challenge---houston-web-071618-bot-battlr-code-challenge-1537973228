dc1086c0c555bf75df06e4d2c06972a017583736
github username <github email address>
2018-09-26 11:33:51 -0500

    almost there

--- src/containers/BotsPage.js
+++ src/containers/BotsPage.js
@@ -1,12 +1,47 @@
 import React from "react";
+import BotCollection from './BotCollection'
+import YourBotArmy from './YourBotArmy'
 
 class BotsPage extends React.Component {
   //start here with your code for step one
+  state = {
+    allTheBots: [],
+    selectedBotId: null,
+    selectedBots: [],
+    removetheBot: false
+  }
+
+  componentDidMount() {
+    fetch('https://bot-battler-api.herokuapp.com/api/v1/bots')
+      .then(r => r.json())
+      .then(data => this.setState({
+        allTheBots: data
+      }))
+  }
+
+  handleClick = (e) => {
+    let bots = this.state.allTheBots
+    let myArray = [...this.state.selectedBots, bots[e.target.id-1]]
+    let selectedBotsss = [...new Set(myArray)]
+    let selectedOrNot = this.state.selectedBoolean
+    this.setState({
+      selectedBotId: e.target.id,
+      selectedBots: selectedBotsss
+    }, () => console.log(this.state))
+  }
+
+  removeBotFromArmy = (e) => {
+    console.log("find the bot to remove from selectedBots, then take it out of the array in state");
+    // find the bot to remove from selectedBots, then take it out of the array in state
+  }
+
+
 
   render() {
     return (
       <div>
-        {/* put your components here */}
+        {<YourBotArmy selectedBots={this.state.selectedBots} removeBotFromArmy={this.removeBotFromArmy} />}
+        {<BotCollection allTheBots={this.state.allTheBots} handleClick={this.handleClick} />}
       </div>
     );
   }
--- src/containers/YourBotArmy.js
+++ src/containers/YourBotArmy.js
@@ -2,21 +2,27 @@ import React from "react";
 import BotCard from "../components/BotCard";
 
 class YourBotArmy extends React.Component {
+
   //your bot army code here...
+  displayTheArmy = () => {
+    return this.props.selectedBots.map(bot => {
+      return <BotCard key={bot.id} details={bot} handleClick={this.props.handleClick} clickedOrNot={true} removeBotFromArmy={this.props.removeBotFromArmy}/>
+    })
+  }
 
   render(){
     return (
       <div className="ui segment inverted olive bot-army">
         <div className="ui five column grid">
           <div className="row bot-army-row">
-            {/*...and here...*/}
+            {this.displayTheArmy()}
             Your Bot Army
           </div>
         </div>
       </div>
     );
   }
-  
+
 };
 
 export default YourBotArmy;
