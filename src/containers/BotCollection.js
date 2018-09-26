27e5fed1ed189153dbacaea7de867085133017ee
Ryhan Chowdhury <ryhanchowdhury@apples-MacBook-Pro-2.local>
2018-09-26 11:23:51 -0500

    submitting code challenge 4

--- src/containers/BotCollection.js
+++ src/containers/BotCollection.js
@@ -1,19 +1,33 @@
 import React from "react";
-import BotCard from "../components/BotCard";
+import BotCard from "../components/BotCard"
 
 class BotCollection extends React.Component {
   //your code here
-
   render(){
+    console.log(this.props)
+
   	return (
   	  <div className="ui four column grid">
     		<div className="row">
-    		  {/*...and here..*/}
-    		  Collection of all bots
+          {this.props.bots.map(bot => {
+            return(
+              <div>
+                <img src={bot.url} alt="robot-picture" onClick={e => this.props.putOnPage(bot)}/>
+                <p>Id:{bot.id}</p>
+                <p>name:{bot.name}</p>
+                <p>health:{bot.health}</p>
+                <p>damage:{bot.damage}</p>
+                <p>armor:{bot.armor}</p>
+                <p>Class:{bot.bot_class}</p>
+                <p>Catchphrase:{bot.catch_phrase}</p>
+              </div>
+            )
+          })}
     		</div>
   	  </div>
   	);
   }
+
 
 };
 
--- src/containers/BotsPage.js
+++ src/containers/BotsPage.js
@@ -1,12 +1,54 @@
 import React from "react";
+import BotCollection from "./BotCollection"
 
 class BotsPage extends React.Component {
   //start here with your code for step one
+  constructor() {
+    super()
+    this.state = {
+      bots: [],
+      loaded: false,
+      botsOnPage: []
+    }
+
+  }
+
+  putOnPage = (bot) => {
+    console.log(bot)
+    this.setState({
+      botsOnPage: [...this.state.botsOnPage, bot]
+    })
+  }
+
+  componentDidMount() {
+    fetch("https://bot-battler-api.herokuapp.com/api/v1/bots")
+      .then(resp => resp.json())
+      .then(data => {
+        const x = data.map(bot => {
+          return {
+            id: bot.id ,
+            name: bot.name,
+            health: bot.health,
+            damage: bot.damage,
+            armor: bot.armor,
+            botClass: bot.bot_class,
+            catchphrase: bot.catchphrase,
+            url: bot.avatar_url
+          }
+        })
+        this.setState({
+          bots: x,
+          loaded: true
+        })
+      })
+  }
+
 
   render() {
+    if (!this.state.loaded) return (<div>Loading...</div>)
     return (
       <div>
-        {/* put your components here */}
+        <BotCollection bots={this.state.bots} putOnPage={this.putOnPage}/>
       </div>
     );
   }
